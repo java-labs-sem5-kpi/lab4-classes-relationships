59d107e273992c10293aecb5e637dc11b1909a12
sergiorbk <sergey24rybak@gmail.com>
2024-10-16 18:05:11 +0300

    Main class improvements were implemented

--- src/main/java/com/sergosoft/sentencessorter/Main.java
+++ src/main/java/com/sergosoft/sentencessorter/Main.java
@@ -4,16 +4,23 @@ import com.sergosoft.sentencessorter.entity.Text;
 import com.sergosoft.sentencessorter.factory.TextFactory;
 import com.sergosoft.sentencessorter.sorter.TextSorter;
 import com.sergosoft.sentencessorter.sorter.impl.CustomTextSorter;
+import com.sergosoft.sentencessorter.sorter.impl.StringBuilderTextSorter;
 
 public class Main {
     public static void main(String[] args) {
-        // Provide text to sort here
-        Text text = TextFactory.createText(new StringBuilder("Sample text to sort. Just try in out here! See ya. Ok?"));
+        StringBuilder textToSortBuilder = new StringBuilder("Sample text to sort. Just try in out here! See ya. Ok?");
+        System.out.println("=> StringBuilder text sorter (lab2)");
+        textSortExample(textToSortBuilder, new StringBuilderTextSorter());
 
+        Text customTextToSort = TextFactory.createText(textToSortBuilder);
+        System.out.println("\n=> Custom text sorter");
+        textSortExample(customTextToSort, new CustomTextSorter());
+    }
+
+    public static <T> void textSortExample(T textToSort, TextSorter<T> textSorter) {
         // processing the text
-        TextSorter<Text> textSorter = new CustomTextSorter();
         try {
-            System.out.println(textSorter.sortByWordCount(text));
+            System.out.println(textSorter.sortByWordCount(textToSort));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
